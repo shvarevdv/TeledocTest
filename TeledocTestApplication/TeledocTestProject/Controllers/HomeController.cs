e89cb04486fc479c67d29877e24347ec171b70ea
shvarevdv <shvarevdmytry@gmail.com>
2020-05-27 11:39:55 +0300

    commit #2

--- TeledocTestApplication/TeledocTestProject/Controllers/HomeController.cs
+++ TeledocTestApplication/TeledocTestProject/Controllers/HomeController.cs
@@ -12,55 +12,11 @@ using TeledocTestProject.Models;
 namespace TeledocTestProject.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-
+    {        
         private DataBaseContext dataBase;
         public HomeController(DataBaseContext dataBase)
         {
-            this.dataBase = dataBase;
-            if (dataBase.Founders.Count() == 0)
-            {
-                //Founder ivanFounder = new Founder { FirstName = "Ivan", SecondName = "Ivanovich", LastName = "Ivanov" };
-                //ivanFounder.SetCreateDate();
-                //ivanFounder.SetUpdateDate();
-                //Founder petrFounder = new Founder { FirstName = "Petr", SecondName = "Petrovich", LastName = "Petrov" };
-                //petrFounder.SetCreateDate();
-                //petrFounder.SetUpdateDate();
-                //Founder viktorFounder = new Founder { FirstName = "Viktor", SecondName = "Viktorovich", LastName = "Petrov" };
-                //viktorFounder.SetCreateDate();
-                //viktorFounder.SetUpdateDate();
-
-                //IndividualEntrepreneur ivanPrivate = new IndividualEntrepreneur { Name = "John" };
-                //ivanPrivate.SetCreateDate();
-                //ivanPrivate.SetUpdateDate();
-                //IndividualEntrepreneur peterPrivate = new IndividualEntrepreneur { Name = "Peter" };
-                //peterPrivate.SetCreateDate();
-                //ivanPrivate.SetUpdateDate();
-                //IndividualEntrepreneur viktorPrivate = new IndividualEntrepreneur { Name = "Viktor" };
-                //peterPrivate.SetCreateDate();
-                //viktorPrivate.SetUpdateDate();
-
-                //EntityClient ivanEntity = new EntityClient { Name = "John" };
-                //ivanEntity.SetCreateDate();
-                //ivanEntity.SetUpdateDate();
-                //ivanEntity.Founders.Append(ivanFounder);
-                //ivanEntity.Founders.Append(petrFounder);
-                //EntityClient peterEntity = new EntityClient { Name = "Peter" };
-                //peterEntity.SetCreateDate();
-                //peterEntity.SetUpdateDate();
-                //peterEntity.Founders.Append(viktorFounder);
-                //peterEntity.Founders.Append(petrFounder);
-                //EntityClient viktorEntity = new EntityClient { Name = "Viktor" };
-                //viktorEntity.SetCreateDate();
-                //viktorEntity.SetUpdateDate();
-                //viktorEntity.Founders.Append(ivanFounder);
-
-                //dataBase.Founders.AddRange(ivanFounder, petrFounder, viktorFounder);
-                //dataBase.IndividualEntrepreneurs.AddRange(ivanPrivate, peterPrivate, viktorPrivate);
-                //dataBase.EntityClients.AddRange(ivanEntity, peterEntity, viktorEntity);
-                //dataBase.SaveChanges();
-            }
+            this.dataBase = dataBase;           
         }
 
         public async Task<IActionResult> Index()
--- TeledocTestApplication/TeledocTestProject/Models/Employe.cs
+++ TeledocTestApplication/TeledocTestProject/Models/Employe.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Text.Json.Serialization;
 
 namespace TeledocTestProject.Models
 {
     public abstract class Employe
     {
         public int Id { get; set; }
+        [JsonPropertyName("ИНН")]
         public int Inn { get; set; }        
         public DateTime CreateDate { get; private set; }
         public DateTime UpdateDate { get; private set; }
